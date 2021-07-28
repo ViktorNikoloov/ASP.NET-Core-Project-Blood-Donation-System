7f44a790f7c597641d496914c89d88752e89f805
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-07-28 20:27:16 +0300

    Add User's service

---
+++ src/Services/BloodDonation.Services.Data/User/IUsersService.cs
@@ -0,0 +1,22 @@
+﻿namespace BloodDonation.Services.Data.User
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using BloodDonation.Data.Models;
+
+    public interface IUsersService
+    {
+        ApplicationUser GetUserByUsername(string username);
+
+        Task AddQuestionsAnswersToUser(QuestionAnswer questionAnswer, ApplicationUser user);
+
+        bool AddUserToRole(string username, string role);
+
+        bool RemoveUserFromRole(string name, string role);
+
+        Recipient GetCurrentSignedInRecipient(string username);
+
+        IEnumerable<ApplicationUser> GetUsersByRole(string role);
+    }
+}
---
+++ src/Services/BloodDonation.Services.Data/User/UsersService.cs
@@ -0,0 +1,73 @@
+﻿using BloodDonation.Data.Common.Repositories;
+using BloodDonation.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodDonation.Services.Data.User
+{
+    public class UsersService
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IDeletableEntityRepository<ApplicationUser> usersRepository;
+
+        public UsersService(
+            IDeletableEntityRepository<ApplicationUser> usersRepository,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+            this.usersRepository = usersRepository;
+        }
+
+        public ApplicationUser GetUserByUsername(string username)
+        {
+            return this.userManager.FindByNameAsync(username).GetAwaiter().GetResult();
+        }
+
+        public bool AddUserToRole(string username, string role)
+        {
+            var user = this.GetUserByUsername(username);
+            if (user == null)
+            {
+                return false;
+            }
+
+            this.userManager.AddToRoleAsync(user, role).GetAwaiter().GetResult();
+            return true;
+        }
+
+        public bool RemoveUserFromRole(string username, string role)
+        {
+            var user = this.GetUserByUsername(username);
+            if (user == null)
+            {
+                return false;
+            }
+
+            this.userManager.RemoveFromRoleAsync(user, role).GetAwaiter().GetResult();
+            return true;
+        }
+
+        public IEnumerable<ApplicationUser> GetUsersByRole(string role)
+        {
+            var usersOfRole = this.userManager.GetUsersInRoleAsync(role).GetAwaiter().GetResult();
+
+            return this.usersRepository.All().Where(x => usersOfRole.Any(u => u.Id == x.Id))
+                                .ToList();
+        }
+
+        public Recipient GetCurrentSignedInOwner(string username)
+        {
+            var user = this.userManager.FindByNameAsync(username).GetAwaiter().GetResult();
+
+            return user.Recipient;
+        }
+
+        public async Task AddQuestionsAnswersToUser(QuestionAnswer questionAnswer, ApplicationUser user)
+        {
+            user.QuestionsAnswers.Add(questionAnswer);
+            await this.usersRepository.SaveChangesAsync();
+        }
+    }
+}
