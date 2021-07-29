a1d007728c8439787de95c135bfbd7d8d59fdc17
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-07-30 02:19:02 +0300

    Start working on Administator panel

---
+++ src/Services/BloodDonation.Services.Data/Administator/AdministratorService.cs
@@ -0,0 +1,55 @@
+﻿namespace BloodDonation.Services.Data.Administator
+{
+    using BloodDonation.Data.Common.Repositories;
+    using BloodDonation.Data.Models;
+    using BloodDonation.Services.Mapping;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class AdministratorService : IAdministratorService
+    {
+        private readonly IDeletableEntityRepository<ApplicationUser> usersRepository;
+        private readonly IDeletableEntityRepository<QuestionAnswer> questionsRepository;
+
+        public AdministratorService(IDeletableEntityRepository<ApplicationUser> usersRepository, IDeletableEntityRepository<QuestionAnswer> questionsRepository)
+        {
+            this.usersRepository = usersRepository;
+            this.questionsRepository = questionsRepository;
+        }
+
+        public async Task AddDonorAsync(string id)
+        {
+            var user = this.usersRepository.All().FirstOrDefault(u => u.Id == id);
+
+            var dogsitter = new Donor();
+            user.Donor = dogsitter;
+            user.Donor.PhoneNumber = user.PhoneNumber;
+
+            await this.usersRepository.SaveChangesAsync();
+        }
+
+        public T ApplicantDetailsById<T>(string id)
+        {
+            var user = this.usersRepository.All()
+                 .Where(u => u.Id == id)
+                 .To<T>().FirstOrDefault();
+
+            return user;
+        }
+
+        public async Task RemoveQuestionsAnswersFromUserAsync(string userId)
+        {
+            var questionAnswers = this.questionsRepository.All().Where(qa => qa.UserId == userId);
+
+            foreach (var questionAnswer in questionAnswers)
+            {
+                this.questionsRepository.Delete(questionAnswer);
+            }
+
+            await this.questionsRepository.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Services/BloodDonation.Services.Data/Administator/IAdministratorService.cs
@@ -0,0 +1,13 @@
+﻿namespace BloodDonation.Services.Data.Administator
+{
+    using System.Threading.Tasks;
+
+    public interface IAdministratorService
+    {
+        Т ApplicantDetailsById<Т>(string id);
+
+        Task RemoveQuestionsAnswersFromUserAsync(string userId);
+
+        Task AddDonorAsync(string id);
+    }
+}
