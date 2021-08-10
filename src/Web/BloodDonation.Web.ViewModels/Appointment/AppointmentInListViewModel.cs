2aa68ce86abb14af154d9ab9a32389970c813b73
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-08-11 01:29:34 +0300

    Implement to show EnumDisplayName

--- src/Web/BloodDonation.Web.ViewModels/Appointment/AppointmentInListViewModel.cs
+++ src/Web/BloodDonation.Web.ViewModels/Appointment/AppointmentInListViewModel.cs
@@ -3,9 +3,8 @@
     using System;
     using System.ComponentModel.DataAnnotations;
 
-    using BloodDonation.Data.Models;
     using BloodDonation.Data.Models.Enums;
-    using BloodDonation.Services.Mapping;
+
 
     public class AppointmentInListViewModel
     {
@@ -14,8 +13,7 @@
         [Display(Name = "Име")]
         public string RecipientFirstName { get; set; }
 
-        [Display(Name = "Кръв")]
-        [EnumDataType(typeof(BloodType))]
+        [Display(Name = "Кръвна група")]
         public BloodType BloodType { get; set; }
 
         [Display(Name = "Банки кръв")]
@@ -28,5 +26,54 @@
         public string AdditionalInfo { get; set; }
 
         public string ImageUrl { get; set; }
+
+        public string EnumDisplayName
+            => this.EnumHelperDisplayName(this.BloodType);
+
+        private string EnumHelperDisplayName(BloodType bloodType)
+        {
+            string enumDisplayName = string.Empty;
+
+            if (bloodType == BloodType.Unknown)
+            {
+                enumDisplayName = "Липсва";
+            }
+            else if (bloodType == BloodType.APositive)
+            {
+                enumDisplayName = "A(+)";
+            }
+            else if (bloodType == BloodType.ANegative)
+            {
+                enumDisplayName = "A(-)";
+            }
+            else if (bloodType == BloodType.BPositive)
+            {
+                enumDisplayName = "B(+)";
+            }
+            else if (bloodType == BloodType.BNegative)
+            {
+                enumDisplayName = "B(-)";
+            }
+            else if (bloodType == BloodType.ABPositive)
+            {
+                enumDisplayName = "AB(+)";
+            }
+            else if (bloodType == BloodType.ABNegative)
+            {
+                enumDisplayName = "AB(-)";
+            }
+            else if (bloodType == BloodType.ZeroPositive)
+            {
+                enumDisplayName = "0(+)";
+            }
+            else if (bloodType == BloodType.ZeroNegative)
+            {
+                enumDisplayName = "0(-)";
+            }
+
+            return enumDisplayName;
+        }
     }
+
+    
 }
